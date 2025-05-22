c35821f79570a4c2feebfda8822b5497a6cb1ae5
shubh240 <shubhamvarma8257@gmail.com>
2025-05-22 23:58:28 +0530

    order details and middleware and cart

--- middleware/isUserAuthenticated.js
+++ middleware/isUserAuthenticated.js
@@ -1,28 +1,33 @@
 import jwt from "jsonwebtoken";
+import { Customer } from "../models/customer.model.js";
+import { SellerUserAuth } from "../models/sellerUserInfo.model.js";
+import { sendResponse } from "../common/index.js";
 
 const isUserAuthenticated = async (req, res, next) => {
   try {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; 
+    const token = authHeader && authHeader.split(' ')[1];
     
     if (!token) {
-      return res
-        .status(401)
-        .json({ auth: false, message: "User is not authenticated" });
+      return sendResponse(res, 400, false, "Token missing");
     }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     if (!decoded || !decoded.userId) {
-      return res.status(401).json({ auth: false, message: "Invalid token" });
+      return sendResponse(res, 401, false, "Invalid token");
     }
-
-    req.id = decoded.userId;
+    /**
+     * Customer-Seller Token Check
+     */
+    const customer = await Customer.findById(decoded.userId);
+    console.log(customer);
+    
+    const seller = await SellerUserAuth.findById(decoded.userId);
+    if(customer) req.id = customer._id;
+    if(seller) req.id = seller._id;
     next();
   } catch (error) {
     console.error("JWT verify error:", error.message);
-    return res
-      .status(401)
-      .json({ auth: false, message: "Token verification failed" });
+    return sendResponse(res, 401, false, error.message);
   }
 };
 
